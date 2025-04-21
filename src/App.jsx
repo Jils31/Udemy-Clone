d3baeaead66d2197ece1b68e4b5747b0efab1e13
Jils Patel <jilspatel21@gmail.com>
2025-04-21 20:41:10 +0530

    Remove unnecessary things from the code

--- src/App.jsx
+++ src/App.jsx
@@ -33,18 +33,6 @@ function App() {
                 <Route path="/course-list" element={<CoursesList />} />
                 <Route path="/course-list/:input" element={<CoursesList />} />
                 <Route path="/course/:id" element={<CourseDetails />} />
-                <Route path="/my-enrollments" element={<MyEnrollments />} />
-                <Route path="/player/:courseID" element={<Player />} />
-
-                <Route path="/educator" element={<Educator />}>
-                  <Route path="educator" element={<Dashboard />} />
-                  <Route path="add-course" element={<AddCourse />} />
-                  <Route path="my-course" element={<MyCourse />} />
-                  <Route
-                    path="student-enrolled"
-                    element={<StudentsEnrolled />}
-                  />
-                </Route>
               </Routes>
             </Suspense>
           }
